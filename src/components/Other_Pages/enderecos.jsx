59dcc2db4c3e891d64ebee308bc0cb9aa21bb9c3
Stephany Oliveira da Silva <stephany16ce@outlook.com>
2025-01-20 16:45:26 -0300

    Alteração no HTML e adição dos profissionais ficticios

---
+++ src/components/Other_Pages/enderecos.jsx
@@ -0,0 +1,35 @@
+import './enderecos.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+
+export default function enderecos() {
+    return (
+        <>
+            <div>
+                <h3>Unidade 1 - Maraponga</h3>
+                <p> Rua das Flores, 123 -  Cidade Azul, Maraponga</p>
+                <button><FontAwesomeIcon icon={faPhone} /></button>
+                <button><FontAwesomeIcon icon={faCalendarDays}/></button>
+            </div>
+            <div>
+                <h3>Unidade 2 - Montese</h3>
+                <p>Avenida dos Sonhos, 456 - Jardim Esperança, Montese</p>
+                <button><FontAwesomeIcon icon={faPhone} /></button>
+                <button><FontAwesomeIcon icon={faCalendarDays}/></button>
+            </div>
+            <div>
+                <h3>Unidade 3 - Aldeota</h3>
+                <p>Travessa do Horizonte, 789 - Bairro das Estrelas, Aldeota</p>
+                <button><FontAwesomeIcon icon={faPhone} /></button>
+                <button><FontAwesomeIcon icon={faCalendarDays}/></button>
+            </div>
+            <div>
+                <h3>Unidade 4 - Benfica</h3>
+                <p> Alameda das Palmeiras, 101 - Parque Sereno, Benfica</p>
+                <button><FontAwesomeIcon icon={faPhone} /></button>
+                <button><FontAwesomeIcon icon={faCalendarDays}/></button>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Other_Pages/profissionais.jsx
@@ -0,0 +1,90 @@
+import './profissionais.css'
+import image1 from './images/psicologa1.jpg'
+import image2 from './images/psicologo2.jpg'
+import image3 from './images/psicologa3.jpg'
+import image4 from './images/psicologa4.jpg'
+import image5 from './images/psicologo5.jpg'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+export default function profissionais() {
+  const cards = [
+    {
+      image: image1,
+      name: 'Dra. Carla Silva',
+      description: 'Especialista em Psicologia Infantil.',
+      buttons: [
+        { label: 'Agendar Consulta' },
+      ],
+    },
+    {
+      image: image2,
+      name: 'Dr. João Augusto',
+      description: 'Psicólogo Clínico com foco em Terapia Cognitivo-Comportamental.',
+      buttons: [
+        { label: 'Agendar Consulta'},
+      ],
+    },
+    {
+      image: image3,
+      name: 'Dra. Ana Costa',
+      description: 'Especialista em Terapia de Casais e Família.',
+      buttons: [
+        { label: 'Agendar Consulta' },
+      ],
+    },
+    {
+      image: image4,
+      name: 'Dra. Fernanda Cavalcante',
+      description: 'Psicóloga Escolar ajudando crianças e adolescentes.',
+      buttons: [
+        { label: 'Agendar Consulta'},
+      ],
+    },
+    {
+      image: image5,
+      name: 'Dr. Ricardo Correia',
+      description: 'Especialista em Psicoterapia e Desenvolvimento Pessoal.',
+      buttons: [
+        { label: 'Agendar Consulta'},
+      ],
+    },
+  ];
+
+
+  return (
+    <div className='cards-container' >
+      <Swiper modules={[Pagination, Navigation]}
+        pagination={{ clickable: false }}
+        navigation
+        spaceBetween={10}
+        slidesPerView={3}>
+
+        {cards.map((card, index) => (
+          <SwiperSlide key={index}>
+            <img src={card.image} className='cards-syle' alt={card.name} />
+
+            <div className="info-style">
+              <h3>{card.name}</h3>
+              <p>{card.description}</p>
+              {/*botao diferente para cada profissional */}
+
+              <div className="card-buttons">
+                {card.buttons.map((button, buttonIndex) => (
+                  <button key={buttonIndex}>
+                    {button.label}
+                  </button>
+                ))}
+              </div>
+
+            </div>
+
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div >
+  )
+}
